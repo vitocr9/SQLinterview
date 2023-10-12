27cd0732ef3f7c5878dcbabb1a80aba6b46ad665
Victorino Cazares <vitocr9@gmail.com>
2023-10-12 12:25:00 -0600

    adding ps1 to run SQL sentences directly to RDS

---
+++ SQLScriptsToRDS.ps1
@@ -0,0 +1,14 @@
+$SQLServer= "interview4bc.cjrvboc3ad46.us-east-1.rds.amazonaws.com,3341"
+$username = "sa"
+$password = 'V3ry5tr0ngP455'
+$files = Get-ChildItem -Path ./SetupSQL
+
+$ConnectionString = "Server=$SQLServer;User Id=$username;Password=$password;TrustServerCertificate=True"
+
+
+foreach ($query in $files){
+    Invoke-Sqlcmd -ConnectionString $ConnectionString -InputFile $query
+
+}
+
+
